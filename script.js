ade663228ab9d9a931e353a48a118919b38a4a7d
MiguelGarcia98GitHub <100066250+MiguelGarcia98GitHub@users.noreply.github.com>
2022-04-09 18:36:51 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const GitHubProyect = document.querySelector("#github-proyect");
+const GitHubProfile = document.querySelector("#github-profile");
+
+GitHubProyect.addEventListener("click", () => {
+  window.open("https://miguelgarcia98github.github.io/turbogreen/")
+})
+
+GitHubProfile.addEventListener("click", () => {
+  window.open("https://github.com/MiguelGarcia98GitHub")
+})
+

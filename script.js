8f6886627839e8c6648de27392304fcc51015597
MiguelGarcia98GitHub <100066250+MiguelGarcia98GitHub@users.noreply.github.com>
2022-04-09 19:01:35 +0200

    Media Query Fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const GitHubProyect = document.querySelector("#github-proyect");
 const GitHubProfile = document.querySelector("#github-profile");
 
 GitHubProyect.addEventListener("click", () => {
-  window.open("https://miguelgarcia98github.github.io/electric-car-website-responsive-for-desktop-and-phones/")
+  window.open("https://github.com/MiguelGarcia98GitHub/electric-car-website-responsive-for-desktop-and-phones")
 })
 
 GitHubProfile.addEventListener("click", () => {
